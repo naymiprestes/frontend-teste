698802da28d224030494809486f6f534e6568649
naymiprestes <naymi.prestes1997@gmail.com>
2023-08-06 22:02:49 -0400

    feat: menu e accordion

--- 3-js/assets/js/main.js
+++ 3-js/assets/js/main.js
@@ -6,5 +6,43 @@
 // Vanilla JS
 
 // window.onload = function() {
-  // code
+//   code
 // };
+
+
+const menu = () => {
+
+  const menuButton = document.querySelector("#menu-button");
+  const menu = document.querySelector(".menu")
+
+    const menuActive = (event) => {
+        if(menu.classList.contains("-active")) {
+          menu.classList.remove("-active")
+        }
+        else {
+          menu.classList.add("-active")
+        }
+      }
+  
+    menuButton.addEventListener("click", menuActive) 
+}
+menu()
+
+const accordion = () => { 
+
+  const itens = document.querySelectorAll(".item")
+
+  itens.forEach(item => {
+     item.addEventListener("click", () => {
+        if(item.classList.contains("-active")) {
+          item.classList.remove("-active")
+        }
+        else {
+          itens.forEach((item) => {
+            item.classList.add("-active")
+          })
+        }
+      })
+     })
+}
+accordion()
